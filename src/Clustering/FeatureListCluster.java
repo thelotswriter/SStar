5033065588430c60ba2602f9b1131abd82a10b4a
thelotswriter <thelotswriter@gmail.com>
2020-04-24 17:25:13 -0600

    FeatureListCluster coded up!

--- src/Clustering/FeatureListCluster.java
+++ src/Clustering/FeatureListCluster.java
@@ -1,63 +1,77 @@
 package Clustering;
 
 import Features.Feature;
+import Features.FeatureList;
 
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class FeatureListCluster
 {
 
-    Set<List<Feature>> featureLists;
-    List<Feature> centroid;
+    private Set<FeatureList> fLists;
+    private FeatureList centroid;
+    private boolean needsRecalculation;
 
-    public FeatureListCluster()
+    public FeatureListCluster(FeatureList featureList)
     {
-        featureLists = new HashSet<>();
-        centroid = null;
+        fLists = new HashSet<>();
+        fLists.add(featureList);
+        centroid = featureList;
+        needsRecalculation = false;
     }
 
-    public void addFeatureList(List<Feature> featureList)
+    public boolean add(FeatureList newFeatureList)
     {
-        featureLists.add(featureList);
-        if(featureLists.size() == 1)
+        needsRecalculation = true;
+        return fLists.add(newFeatureList);
+    }
+
+    public boolean contains(FeatureList featureList)
+    {
+        return fLists.contains(featureList);
+    }
+
+    public void remove(FeatureList featureList)
+    {
+        needsRecalculation = true;
+        fLists.remove(featureList);
+        if(featureList == centroid)
         {
-            centroid = featureList;
-        } else
-        {
-            calculateCentroid();
+            recalculateCentroid();
         }
     }
 
-    public void removeFeatureList(List<Feature> featureList)
-    {
-        featureLists.remove(featureList);
-    }
-
-    public List<Feature> getCentroid()
+    public FeatureList getCentroid()
     {
         return centroid;
     }
 
-    public int size()
+    public void recalculateCentroid()
     {
-        return featureLists.size();
-    }
-
-    public boolean isEmpty()
-    {
-        return featureLists.isEmpty();
-    }
-
-    public void clear()
-    {
-        featureLists.clear();
-    }
-
-    private void calculateCentroid()
-    {
-        // TODO: This
+        if(needsRecalculation)
+        {
+            FeatureList currentBest = null;
+            int maxTotalShared = 0;
+            for(FeatureList featureList : fLists)
+            {
+                int totalShared = 0;
+                for(FeatureList otherFeatureList :fLists)
+                {
+                    if (featureList != otherFeatureList)
+                    {
+                        totalShared += featureList.getNumMatches(otherFeatureList);
+                    }
+                }
+                if(totalShared > maxTotalShared)
+                {
+                    maxTotalShared = totalShared;
+                    currentBest = featureList;
+                }
+            }
+            centroid = currentBest;
+        }
     }
 
 }
--- src/Clustering/FeatureListToClusters.java
+++
@@ -1,4 +0,0 @@
-package Clustering;
-
-public class FeatureListToClusters {
-}
